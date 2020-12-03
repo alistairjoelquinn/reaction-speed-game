21fba3a1ebd0fc3d62116b18fa6b1a0dd914037c
Alistair Quinn <alistairjoelquinn@gmail.com>
2020-12-03 20:00:41 +0100

    start.js set up for using redux with redux thunk

--- src/start.js
+++ src/start.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 import App from './app';
+import Reducer from '../store/reducer';
 
-ReactDOM.render(<App />, document.querySelector('main'));
+const store = createStore(
+    Reducer, composeWithDevTools(
+        applyMiddleware(ReduxThunk)
+    )
+);
+
+const elem = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(elem, document.querySelector('main'));
