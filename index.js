6479b7270a949d15127e5c53855705ea0866870b
Alistair Quinn <alistairjoelquinn@gmail.com>
2020-12-04 21:55:05 +0100

    socket.io on server connected with redux store

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const compression = require('compression');
+const cookieSession = require('cookie-session');
+const server = require('http').Server(app);
+const io = require('socket.io')(server, { origins: 'localhost:8080' });
 const consola = require('consola');
+const cookieSessionMiddleware = cookieSession({
+    secret: `It's been emotional`,
+    maxAge: 1000 * 60 * 60 * 24 * 90
+});
 
 app.use(compression());
+app.use(cookieSessionMiddleware);
+io.use(function (socket, next) {
+    cookieSessionMiddleware(socket.request, socket.request.res, next);
+});
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(
@@ -18,4 +29,18 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.get('*', (req, res) => res.sendFile(__dirname + '/init/index.html'));
 
-app.listen(8080, () => consola.success("Server Listening"));
+server.listen(8080, () => consola.success("Server Listening"));
+
+io.on('connection', (socket) => {
+    if (!socket.request.session.userId) return socket.disconnect(true);
+    const { userId } = socket.request.session;
+    onlineUsers[userId] = socket.id;
+
+    updateOnlineUsers(Object.keys(onlineUsers))
+        .then(({ rows }) => {
+            io.sockets.emit('onlineUsers', rows);
+        })
+        .catch(err => console.error('Error updating online users: ', err));
+
+
+});
