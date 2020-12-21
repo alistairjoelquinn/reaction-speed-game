705f7b57e2459997d6aa00c0aa5de46e2422df6c
Alistair Quinn <alistairjoelquinn@gmail.com>
2020-12-21 20:50:11 +0100

    ready to deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const compression = require('compression');
 const server = require('http').Server(app);
-const io = require('socket.io')(server, { origins: 'localhost:8080' });
+const io = require('socket.io')(server, { origins: 'reaction-speed-game.herokuapp.com' });
 const { selectedColorsCalculate } = require('./utils/selectedColorsCalculate');
 
 app.use(compression());
